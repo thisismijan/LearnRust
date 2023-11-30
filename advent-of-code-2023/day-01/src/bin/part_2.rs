c2bb4f6a6dbb97d26e28e4496e2dd74f1c70ba97
thisismijan <00.gaits_fierce@icloud.com>
2023-11-30 20:05:32 +0000

    aoc init

---
+++ advent-of-code-2023/day-01/src/bin/part_2.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let input = include_str!("input.txt").unwrap();
+    let output = process(input);
+    dbg!(output);
+}
+
+fn process (input: &str) {
+
+}
+
+#[cfg(test)]
+#[test]
+fn test_process () {
+
+}
