4803a11261dcfee33aaf82c936691b0b3d638334
thisismijan <00.gaits_fierce@icloud.com>
2023-12-01 19:39:12 +0000

    day1

--- advent-of-code-2023/day-01/src/bin/part_1.rs
+++ advent-of-code-2023/day-01/src/bin/part_1.rs
@@ -1,15 +1,39 @@
 fn main() {
-    let input = include_str!("input.txt").unwrap();
+    let input = include_str!("input1.txt");
     let output = process(input);
     dbg!(output);
 }
 
-fn process (input: &str) {
+fn process(input: &str) -> u32 {
+    let output = input
+        .lines()
+        .map(|line| {
+            let mut it = line.chars().filter_map(|c| c.to_digit(10));
+            let first = it.next().unwrap();
+            let last = it.last();
+            match last {
+                Some(num) => format!("{}{}", first, num),
+                None => format!("{}{}", first, first),
+            }
+            .parse::<u32>()
+            .unwrap()
+        })
+        .sum::<u32>();
 
+    return output;
 }
 
 #[cfg(test)]
-#[test]
-fn test_process () {
+mod tests {
+    use super::*;
+    #[test]
+    fn test_process() {
+        let input = "1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet";
 
+        let result = process(input);
+        assert_eq!(142, result);
+    }
 }
--- advent-of-code-2023/day-01/src/bin/part_2.rs
+++ advent-of-code-2023/day-01/src/bin/part_2.rs
@@ -1,15 +1,66 @@
 fn main() {
-    let input = include_str!("input.txt").unwrap();
+    let input = include_str!("input1.txt");
     let output = process(input);
     dbg!(output);
 }
 
-fn process (input: &str) {
+fn process(input: &str) -> u32 {
+    let output = input
+        .lines()
+        .map(|line| {
+            let mut it = (0..line.len()).filter_map(|i| {
+                let line_substr = &line[i..];
+                let result = if line_substr.starts_with("one") {
+                    '1'
+                } else if line_substr.starts_with("two") {
+                    '2'
+                } else if line_substr.starts_with("three") {
+                    '3'
+                } else if line_substr.starts_with("four") {
+                    '4'
+                } else if line_substr.starts_with("five") {
+                    '5'
+                } else if line_substr.starts_with("six") {
+                    '6'
+                } else if line_substr.starts_with("seven") {
+                    '7'
+                } else if line_substr.starts_with("eight") {
+                    '8'
+                } else if line_substr.starts_with("nine") {
+                    '9'
+                } else {
+                    line_substr.chars().next().unwrap()
+                };
+                result.to_digit(10)
+            });
+            let first = it.next().unwrap();
+            let last = it.last();
+            match last {
+                Some(num) => format!("{}{}", first, num),
+                None => format!("{}{}", first, first),
+            }
+            .parse::<u32>()
+            .unwrap()
+        })
+        .sum::<u32>();
 
+    return output;
 }
 
 #[cfg(test)]
-#[test]
-fn test_process () {
+mod tests {
+    use super::*;
+    #[test]
+    fn test_process() {
+        let input = "two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen";
 
+        let result = process(input);
+        assert_eq!(281, result);
+    }
 }
