b4c4df1061024dd8e1790001929e608aa2c42bfb
Mijan Ahmed <mijan.ahmed@hotmail.co.uk>
2023-12-14 21:27:23 +0000

    day-04

---
+++ advent-of-code-2023/day-04/src/bin/part_2.rs
@@ -0,0 +1,104 @@
+use std::collections::{BTreeMap, HashMap, HashSet};
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, digit1, line_ending, space0, space1},
+    multi::{fold_many1, separated_list1},
+    sequence::{delimited, separated_pair, terminated, tuple},
+    IResult, Parser,
+};
+
+fn main() {
+    let input = include_str!("input1.txt");
+    let result = process(input);
+    println!("Result: {}", result);
+}
+
+#[derive(Debug)]
+struct Card {
+    winning_numbers: HashSet<u32>,
+    my_numbers: HashSet<u32>,
+}
+
+fn parse_cards(input: &str) -> IResult<&str, Vec<Card>> {
+    separated_list1(line_ending, parse_card)(input)
+}
+
+fn parse_card(input: &str) -> IResult<&str, Card> {
+    let (input, _) = delimited(
+        tuple((tag("Card"), space1)),
+        digit1,
+        tuple((tag(":"), space1)),
+    )(input)?;
+    separated_pair(parse_numbers, tuple((tag("|"), space1)), parse_numbers)
+        .map(|(winning_numbers, my_numbers)| Card {
+            winning_numbers,
+            my_numbers,
+        })
+        .parse(input)
+}
+
+fn parse_numbers(input: &str) -> IResult<&str, HashSet<u32>> {
+    fold_many1(
+        terminated(complete::u32, space0),
+        HashSet::new,
+        |mut acc, item| {
+            acc.insert(item);
+            acc
+        },
+    )(input)
+}
+
+fn process(input: &str) -> u32 {
+    let cards = match parse_cards(input) {
+        Ok((_, cards)) => cards,
+        Err(err) => {
+            eprintln!("Error parsing input: {:?}", err);
+            return 0;
+        }
+    };
+
+    dbg!(&cards);
+
+    let matches_counts = cards
+        .iter()
+        .map(|card| card.winning_numbers.intersection(&card.my_numbers).count() as u32)
+        .collect::<Vec<u32>>();
+
+    let card_count = (0..matches_counts.len())
+        .map(|i| (i, 1))
+        .collect::<BTreeMap<usize, u32>>();
+
+    matches_counts
+        .iter()
+        .enumerate()
+        .fold(card_count, |mut acc, (index, card_count)| {
+            for i in (index + 1)..(index + 1 + *card_count as usize) {
+                let additional_card_count = *acc.get(&index).unwrap();
+                acc.entry(i).and_modify(|value| {
+                    *value += additional_card_count;
+                });
+            }
+            acc
+        })
+        .values()
+        .sum::<u32>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+        let result = process(input);
+        assert_eq!(30, result);
+    }
+}
