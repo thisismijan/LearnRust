27e7b395c9a67266de111972261f1e2b61c0c6fb
Mijan Ahmed <mijan.ahmed@hotmail.co.uk>
2023-12-28 17:44:57 +0000

    day-11

---
+++ advent-of-code-2023/day-11/src/bin/part_2.rs
@@ -0,0 +1,89 @@
+use std::iter::from_fn;
+
+use itertools::Itertools;
+
+fn main() {
+    let input = include_str!("input1.txt");
+    let result = process(input);
+    println!("Result: {}", result);
+}
+
+fn process(input: &str) -> i64 {
+    let points = parse_input(input);
+    points
+        .iter()
+        .combinations(2)
+        .map(|points| {
+            (points[1].0 as i64 - points[0].0 as i64).abs()
+                + (points[1].1 as i64 - points[0].1 as i64).abs()
+        })
+        .sum::<i64>()
+}
+
+fn parse_input(input: &str) -> Vec<(usize, usize)> {
+    let empty_rows = input
+        .lines()
+        .enumerate()
+        .filter_map(|(y, row)| row.chars().all(|c| c == '.').then_some(y))
+        .collect::<Vec<usize>>();
+    let mut columns = input.lines().map(|line| line.chars()).collect::<Vec<_>>();
+    let empty_columns = from_fn(move || {
+        let mut items = vec![];
+        for iter in &mut columns {
+            match iter.next() {
+                Some(item) => {
+                    items.push(item);
+                }
+                None => return None,
+            }
+        }
+        Some(items)
+    })
+    .enumerate()
+    .filter_map(|(x, column)| column.iter().all(|c| c == &'.').then_some(x))
+    .collect::<Vec<usize>>();
+    let mut result = Vec::new();
+
+    let mut y_inc = 0;
+    input.lines().enumerate().for_each(|(y, line)| {
+        let mut yinc = false;
+        if empty_rows.contains(&y) & !yinc {
+            y_inc += 999999;
+            yinc = true;
+        }
+        let mut x_inc = 0;
+        line.chars().enumerate().for_each(|(x, c)| {
+            let mut xinc = false;
+            if empty_columns.contains(&x) & !xinc {
+                x_inc += 999999;
+                xinc = true;
+            }
+            if c.eq_ignore_ascii_case(&'#') {
+                result.push((x + x_inc, y + y_inc));
+            }
+        })
+    });
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+
+        let result = process(input);
+        assert_eq!(82000210, result);
+    }
+}
